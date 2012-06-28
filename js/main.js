51b9edbb983e815f6b8cae02250ca6eaf11e337e
Kai Hendry <hendry@webconverger.com>
2012-06-28 12:33:16 +0200

    basic works

--- js/main.js
+++ js/main.js
@@ -1,49 +1,26 @@
-google.load("visualization", "1", {
-	packages: ["corechart"]
+// Load the Visualization API and the piechart package.
+google.load('visualization', '1', {
+	'packages': ['corechart']
 });
+
+// Set a callback to run when the Google Visualization API is loaded.
 google.setOnLoadCallback(drawChart);
+
 function drawChart() {
+	var jsonData = $.ajax({
+		url: "/sample.json",
+		dataType: "json",
+		async: false
+	}).responseText;
 
-	function downloadUrl(url, callback) {
-		var request = window.ActiveXObject ? new ActiveXObject('Microsoft.XMLHTTP') : new XMLHttpRequest();
-		request.onreadystatechange = function() {
-			if (request.readyState == 4) {
-				callback(request.responseText, request.status);
-			}
-		};
-		request.open('GET', url, true);
-		request.send(null);
-	}
+	// Create our data table out of JSON data loaded from server.
+	var data = new google.visualization.DataTable(jsonData);
 
-	var data = new google.visualization.DataTable();
-	data.addColumn('string', 'Month');
-	downloadUrl("header.json", header);
-
-	function header(arraystring) {
-		console.log(arraystring);
-		arr = JSON.parse(arraystring);
-		for (var i = 0; i < arr.length; i++) {
-			console.log(arr[i]);
-			data.addColumn('number', arr[i]);
-		}
-
-		downloadUrl("total.json", rows);
-	}
-
-	function rows(rowstring) {
-
-		console.log(rowstring);
-		data.addRows(JSON.parse(rowstring));
-
-		var options = {
-			width: window.innerWidth,
-			height: window.innerHeight,
-			title: 'Webconverger usage'
-		};
-
-		var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-		chart.draw(data, options);
-
-	}
+	// Instantiate and draw our chart, passing in some options.
+	var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+	chart.draw(data, {
+		width: 400,
+		height: 240
+	});
 }
 
