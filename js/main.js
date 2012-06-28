ca294e029fd55933d26c8dceb16c44ed5e7a1656
Kai Hendry <hendry@webconverger.com>
2012-06-28 13:41:21 +0200

    works

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Load the Visualization API and the piechart package.
 google.load('visualization', '1', {
-	'packages': ['corechart']
+	'packages': ['annotatedtimeline']
 });
 
 // Set a callback to run when the Google Visualization API is loaded.
@@ -13,14 +13,12 @@ function drawChart() {
 		async: false
 	}).responseText;
 
-	// Create our data table out of JSON data loaded from server.
-	var data = new google.visualization.DataTable(jsonData);
+// Create our data table out of JSON data loaded from server.
+var data = new google.visualization.DataTable(jsonData);
 
-	// Instantiate and draw our chart, passing in some options.
-	var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-	chart.draw(data, {
-		width: 400,
-		height: 240
-	});
+var chart = new google.visualization.AnnotatedTimeLine(document.getElementById('chart_div'));
+
+chart.draw(data, {width: 640, height: 480});
+
 }
 
